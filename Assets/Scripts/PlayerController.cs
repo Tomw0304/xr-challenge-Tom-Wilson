517e1bf2b0a4266eefb297349e95aecc338995a6
Tom Wilson <tomronwil@gmail.com>
2023-12-20 18:02:47 +0000

    Update PlayerController.cs
    Added the moving forward code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,6 +17,19 @@ public class PlayerController : MonoBehaviour
     // Update is at 50 times per second
     void FixedUpdate()
     {
-        
+        // Move forward
+        if (Input.GetKey(KeyCode.W))
+        {
+            // Use velovity at lower speed to create an instant start and at the max speed to cap the speed
+            if (rb.velocity.magnitude < 5f || rb.velocity.magnitude = 100f)
+            {
+                rb.velocity = transform.forward * 100;
+            }
+            // Accelerate inbetween the start and reaching a top speed
+            else 
+            {
+                rb.AddForce(transform.forward * 10000);
+            }
+        }
     }
 }
