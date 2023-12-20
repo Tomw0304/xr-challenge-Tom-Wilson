945d0c8f56b083d9c7b8e4047522d232e1a09654
Tom Wilson <tomronwil@gmail.com>
2023-12-20 18:25:08 +0000

    Update PlayerController.cs
    Changed it so that the max speed is limited by a less then

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,12 +21,12 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             // Use velovity at lower speed to create an instant start and at the max speed to cap the speed
-            if (rb.velocity.magnitude < 5f || rb.velocity.magnitude == 100f)
+            if (rb.velocity.magnitude < 5f)
             {
                 rb.velocity = transform.forward * 100;
             }
             // Accelerate inbetween the start and reaching a top speed
-            else 
+            else if (rb.velocity.magnitude < 100f)
             {
                 rb.AddForce(transform.forward * 10000);
             }
