bf7bdc008bffcf4c5375e30aa210a1404860bb7c
Tom Wilson <tomronwil@gmail.com>
2023-12-21 00:25:49 +0000

    Update PlayerController.cs
    Added turning left code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,12 @@ public class PlayerController : MonoBehaviour
     // Create variable to store the rigidbody of the player
     private Rigidbody rb;
 
+    // Initialise a boolean variable to store whether the player is rotated
+    private bool rotatingLeft = false;
+
+    // Initialise a variable to store the time since the start of the rotation
+    private float rotatingTime = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +42,41 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
         {
             rb.AddForce(-transform.forward * 1000);
+        }
+
+        // Turns left
+        if (Input.GetKey(KeyCode.A))
+        {
+            rotatingLeft = true;
+        }
+
+
+        // Resets the rotation variable when the rotation is complete
+        if (rotatingTime >= 0.1f)
+        {
+            rotatingLeft = false;
+            rotatingTime = 0.0f;
+        }
+        // Rotating the player 45 degrees to the left for a specific time
+        if (rotatingLeft && rotatingTime < 0.1f)
+        {
+            // Increment the rotatingTime
+            rotatingTime += Time.deltaTime;
+
+            // Calculate the interpolation factor based on the fraction of the rotating time elapsed
+            float t = Mathf.Clamp01(rotatingTime / 0.1f);
+
+            // Initalise the current rotation to the player's current rotation
+            Vector3 currentRotation = transform.rotation.eulerAngles;
+
+            // Minus 45 degrees to the Y axis rotation
+            currentRotation.y -= 45;
+
+            // Create euler rotation with transformed angle
+            Quaternion targetRotation = Quaternion.Euler(currentRotation);
+
+            // Rotate the player to the targetRotation
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, t);        
         }
     }
 }
