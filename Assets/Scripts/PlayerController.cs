b8f23ab3b6dd1f03579a4af228e1930413b9f9f2
Tom Wilson <tomronwil@gmail.com>
2023-12-23 00:16:53 +0000

    Update PlayerController.cs
    Adds conditions to check whether the velocity is moving forward when accelerating with w key.
    Adds condition to s key so that it decelerates slowly when the player is moving forward and when the player is moving backward the backward deceleration is increased.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -29,7 +29,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             // Use velovity at lower speed to create an instant start and at the max speed to cap the speed
-            if (rb.velocity.magnitude < 1f)
+            if ((Vector3.Dot(rb.velocity.normalized, transform.forward.normalized) > 0.5f) && rb.velocity.magnitude < 1f)
             {
                 rb.velocity = transform.forward;
             }
@@ -43,7 +43,14 @@ public class PlayerController : MonoBehaviour
         // Move backward
         if (Input.GetKey(KeyCode.S))
         {
-            rb.AddForce(-transform.forward * 10);
+            if (Vector3.Dot(rb.velocity.normalized, transform.forward.normalized) > 0.5f)
+            {
+                rb.AddForce(-transform.forward);
+            }
+            else if ((Vector3.Dot(rb.velocity.normalized, -transform.forward.normalized) > 0.5f) && rb.velocity.magnitude < 10f)
+            {
+                rb.AddForce(-transform.forward * 10);
+            }
         }
 
         // Turns left
