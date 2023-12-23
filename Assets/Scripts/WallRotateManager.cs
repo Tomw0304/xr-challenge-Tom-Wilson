d38282e13f72fb3f0a470000442f8e225518ce0e
Tom Wilson <tomronwil@gmail.com>
2023-12-23 17:01:56 +0000

    Update WallRotateManager.cs
    Added code that when an object collides with a wall it rotates the object 90 degrees

--- Assets/Scripts/WallRotateManager.cs
+++ Assets/Scripts/WallRotateManager.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class WallRotateManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Runs when a collision occurs
+    void OnCollisionEnter(Collision collisionObject)
     {
-        
-    }
+        // Create a new variable to store the rigidbody of the collision object
+        Rigidbody rb = collisionObject.collider.GetComponent<Rigidbody>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Checks if the collision object has a transform and a rigibody
+        if (collisionObject.transform != null && rb != null)
+        {
+            Vector3 currentVelocity = rb.velocity;
+            collisionObject.transform.rotation = Quaternion.Euler(-90f, 0f, 0f) * collisionObject.transform.rotation;
+            rb.velocity = Quaternion.Euler(-90f, 0f, 0f) * rb.velocity;
+        }
     }
 }
