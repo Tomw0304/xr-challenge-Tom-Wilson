c91aca8c018d911b636b4df917a8b921f82e752c
Tom Wilson <tomronwil@gmail.com>
2023-12-30 00:33:00 +0000

    Update WallRotateManager.cs
    Simplified code for rotation when the player is driving forward directly or rotated 180

--- Assets/Scripts/WallRotateManager.cs
+++ Assets/Scripts/WallRotateManager.cs
@@ -10,12 +10,34 @@ public class WallRotateManager : MonoBehaviour
         // Create a new variable to store the rigidbody of the collision object
         Rigidbody rb = collisionObject.GetComponent<Rigidbody>();
 
+        //stores the angle that the object needs to be rotated by
+        float angleDifferenceX = -0f;
+        float angleDifferenceY = 0f;
+
         // Checks if the collision object has a transform and a rigibody
         if (collisionObject.transform != null && rb != null)
         {
+            if(collisionObject.transform.rotation.y == 1f)
+            {
+                collisionObject.transform.rotation = Quaternion.Euler(
+                    collisionObject.transform.rotation.eulerAngles.x,
+                    -collisionObject.transform.rotation.eulerAngles.y,
+                    collisionObject.transform.rotation.eulerAngles.z
+                );
+            }
+            //Calculates the angleDifference based on the angle the object is colliding with the wall
+            if (collisionObject.transform.rotation.z < 0 || collisionObject.transform.rotation.y < 0)
+            {
+                angleDifferenceX = 90f;
+            }
+            else if (collisionObject.transform.rotation.y == 0)
+            {
+                angleDifferenceX = -90f;
+            }
+
             Vector3 currentVelocity = rb.velocity;
-            collisionObject.transform.rotation = Quaternion.Euler(-90f, 0f, 0f) * collisionObject.transform.rotation;
-            rb.velocity = Quaternion.Euler(-90f, 0f, 0f) * rb.velocity;
+            collisionObject.transform.rotation = Quaternion.Euler(angleDifferenceX, angleDifferenceY, 0f) * collisionObject.transform.rotation;
+            rb.velocity = Quaternion.Euler(angleDifferenceX, angleDifferenceY, 0f) * rb.velocity;
         }
     }
 }
