02572ec4ba903401aa83bf51c14f437fe7fc4cef
Tom Wilson <tomronwil@gmail.com>
2023-12-31 00:07:17 +0000

    Update WallRotateManager.cs
    Update code to fix the negation issue on all rotations.
    Update code to calculate the negation on all values.
    Adds conditions to account for rotating right and left.
    Removed unused currentVelocity variable.
    Remove unnecessary negation in the initialisation of angleDifferenceX.
    Added the angleDifference for Z

--- Assets/Scripts/WallRotateManager.cs
+++ Assets/Scripts/WallRotateManager.cs
@@ -11,22 +11,29 @@ public class WallRotateManager : MonoBehaviour
         Rigidbody rb = collisionObject.GetComponent<Rigidbody>();
 
         //stores the angle that the object needs to be rotated by
-        float angleDifferenceX = -0f;
+        float angleDifferenceX = 0f;
         float angleDifferenceY = 0f;
+        float angleDifferenceZ = 0f;
 
         // Checks if the collision object has a transform and a rigibody
         if (collisionObject.transform != null && rb != null)
         {
-            if(collisionObject.transform.rotation.y == 1f)
+            // Checks if the object has returned to the original rotation with a negative value
+            if (collisionObject.transform.rotation == new Quaternion(0f, 0f, 0f, -1f) || collisionObject.transform.rotation == new Quaternion(0f, 1f, 0f, 0f) || collisionObject.transform.rotation == new Quaternion(0f, 0.7071068f, 0f, -0.7071068f) || collisionObject.transform.rotation == new Quaternion(0f, -0.7071068f, 0f, -0.7071068f))
             {
-                collisionObject.transform.rotation = Quaternion.Euler(
-                    collisionObject.transform.rotation.eulerAngles.x,
-                    -collisionObject.transform.rotation.eulerAngles.y,
-                    collisionObject.transform.rotation.eulerAngles.z
-                );
+                // reverse the negation of the rotation
+                collisionObject.transform.rotation = new Quaternion(-collisionObject.transform.rotation.x, -collisionObject.transform.rotation.y, -collisionObject.transform.rotation.z, -collisionObject.transform.rotation.w);
             }
             //Calculates the angleDifference based on the angle the object is colliding with the wall
-            if (collisionObject.transform.rotation.z < 0 || collisionObject.transform.rotation.y < 0)
+            if (collisionObject.transform.rotation == new Quaternion(0f,-0.7071068f,0f,0.7071068f) || collisionObject.transform.rotation == new Quaternion(-0.5f, -0.5f, -0.5f, 0.5f) || collisionObject.transform.rotation == new Quaternion(-0.7071068f, 0f, -0.7071068f, 0f) || collisionObject.transform.rotation == new Quaternion(-0.5f, 0.5f, -0.5f, -0.5f))
+            {
+                angleDifferenceZ = -90f;
+            }
+            else if (collisionObject.transform.rotation == new Quaternion(0f, 0.7071068f, 0f, 0.7071068f) || collisionObject.transform.rotation == new Quaternion(-0.5f, 0.5f, 0.5f, 0.5f) || collisionObject.transform.rotation == new Quaternion(-0.7071068f, 0f, 0.7071068f, 0f) || collisionObject.transform.rotation == new Quaternion(-0.5f, -0.5f, 0.5f, -0.5f))
+            {
+                angleDifferenceZ = 90f;
+            }
+            else if (collisionObject.transform.rotation.z < 0 || collisionObject.transform.rotation.y < 0)
             {
                 angleDifferenceX = 90f;
             }
@@ -35,9 +42,9 @@ public class WallRotateManager : MonoBehaviour
                 angleDifferenceX = -90f;
             }
 
-            Vector3 currentVelocity = rb.velocity;
-            collisionObject.transform.rotation = Quaternion.Euler(angleDifferenceX, angleDifferenceY, 0f) * collisionObject.transform.rotation;
-            rb.velocity = Quaternion.Euler(angleDifferenceX, angleDifferenceY, 0f) * rb.velocity;
+            // applies the angle differences to the object transform and velocity
+            collisionObject.transform.rotation = Quaternion.Euler(angleDifferenceX, angleDifferenceY, angleDifferenceZ) * collisionObject.transform.rotation;
+            rb.velocity = Quaternion.Euler(angleDifferenceX, angleDifferenceY, angleDifferenceZ) * rb.velocity;
         }
     }
 }
