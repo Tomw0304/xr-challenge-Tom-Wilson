18d789313ed62f8c034bb0dfa2c069f79e144bcb
Tomw0304 <114393558+Tomw0304@users.noreply.github.com>
2024-01-22 21:46:14 +0200

    Update GameManager.cs
    Added functionality to enable and disable pause UI when pausing and resuming

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,11 @@ public class GameManager : MonoBehaviour
 {
     // Boolean to store whether the game is paused
     private bool paused = false;
-    
+
+    // Variables to store the game UI and the pause UI
+    public GameObject gameUI;
+    public GameObject pauseUI;
+
     // Update is called once per frame
     void Update()
     {
@@ -16,11 +20,15 @@ public class GameManager : MonoBehaviour
             {
                 paused = true;
                 Time.timeScale = 0;
+                gameUI.SetActive(false);
+                pauseUI.SetActive(true);
             }
             else
             {
                 paused = false;
                 Time.timeScale = 1;
+                gameUI.SetActive(true);
+                pauseUI.SetActive(false);
             }
         }
     }
