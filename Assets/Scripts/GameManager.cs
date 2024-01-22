5adc020828fb1e7014f0548c9d9c5ffa2054338c
Tomw0304 <114393558+Tomw0304@users.noreply.github.com>
2024-01-22 22:00:36 +0200

    Update GameManager.cs
    Added comments. Moved pausing functionality to a separate function.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,22 +14,31 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Toggles pausing based on escape button
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if(!paused)
-            {
-                paused = true;
-                Time.timeScale = 0;
-                gameUI.SetActive(false);
-                pauseUI.SetActive(true);
-            }
-            else
-            {
-                paused = false;
-                Time.timeScale = 1;
-                gameUI.SetActive(true);
-                pauseUI.SetActive(false);
-            }
+            TogglePause();
+        }
+    }
+
+    // Toggles the pausing of the game
+    public void TogglePause()
+    {
+        // Pauses the game and changes the UI to the pause UI if the game is not paused
+        if(!paused)
+        {
+            paused = true;
+            Time.timeScale = 0;
+            gameUI.SetActive(false);
+            pauseUI.SetActive(true);
+        }
+        // Resumes the game and changes the UI to the game UI if the game is paused
+        else
+        {
+            paused = false;
+            Time.timeScale = 1;
+            gameUI.SetActive(true);
+            pauseUI.SetActive(false);
         }
     }
 }
